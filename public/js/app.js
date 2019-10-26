c3f3f6d5239c63b0d34896984dd6e7644f6138cb
sarang-sapre <57029965+sarang-sapre@users.noreply.github.com>
2019-10-27 00:18:27 +0530

    Changes

--- public/js/app.js
+++ public/js/app.js
@@ -3,6 +3,8 @@ var inputLocation = document.querySelector('form')
 var search = document.querySelector('input')
 var message1 = document.querySelector('#msg-1')
 var message2 = document.querySelector('#msg-2')
+var message3 = document.querySelector('#msg-3')
+var message4 = document.querySelector('#msg-4')
 
 inputLocation.addEventListener('submit',(e) =>{
     e.preventDefault()
@@ -10,12 +12,16 @@ inputLocation.addEventListener('submit',(e) =>{
     var location = search.value
     message1.textContent = 'Loading........'
     message2.textContent = ''
+    message3.textContent = ''
+    message4.textContent = ''
     fetch('/weather?address=' + location).then((response)=>{
 
     response.json().then((data) => {
     if(!data.error){
         message1.textContent=data.location
         message2.textContent=data.summary
+        message3.textContent =data.temperature
+        message4.textContent =data.pressure
     }else{
         message1.textContent=data.error
     }
